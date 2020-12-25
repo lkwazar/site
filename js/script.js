26d3d03edf60881acb11ed0f7bc214596a788dd0
lkwazar <75472596+lkwazar@users.noreply.github.com>
2020-12-25 17:24:46 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,56 +7,58 @@ let isMobile = {
     any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.IOS() || isMobile.Opera() || isMobile.Windows() ); }
 };
 
-const body = document.querySelector('body');
-
-if(isMobile.any()) {
-    body.classList.add('touch');
-}else{
-    body.classList.add('mouse');
-}
-
 // =================================================================================
 
 // <Меню по клику>
 const btnBurger = document.querySelector('.main-menu__burger');
 const menu =  document.querySelector('.main-menu');
-const toggleMenu = function() {
-    menu.classList.toggle('open');
-}
-const toggleBurger = function() {
-    btnBurger.classList.toggle('active');
-}
+const body = document.querySelector('body');
+const accordion = document.querySelectorAll('.accordion');
+const toggleMenu = function() {menu.classList.toggle('open');}
+const toggleBurger = function() {btnBurger.classList.toggle('active');}
 
-function touchArrow () {
+if(isMobile.any()) {
+    body.classList.add('touch');
     let arrow=document.querySelectorAll('.arrow_touch');
 
-    if(isMobile.any()) {
-        btnBurger.addEventListener('click', function(e) {
-            e.stopPropagation();
-            toggleBurger();
-            toggleMenu();
-            body.classList.toggle('lock');
-            
-        });
-    }
+    btnBurger.addEventListener('click', function(e) {
+        e.stopPropagation();
+        toggleBurger();
+        toggleMenu();
+        body.classList.toggle('lock');
+        
+    });
 
     for(i=0; i<arrow.length; i++) {
         let thisLink=arrow[i].previousElementSibling;
         let subMenu=arrow[i].nextElementSibling;
         let thisArrow=arrow[i];
 
-        if(isMobile.any()) {
-            thisLink.classList.add('parent');
-        }
+        thisLink.classList.add('parent');
         
         arrow[i].addEventListener('click', function() {
             subMenu.classList.toggle('open');
             thisArrow.classList.toggle('active');
         });
     }
+
+}else{
+    body.classList.add('mouse');
 }
-touchArrow();
 // </Меню по клику>
+
+// =================================================================================
+
+// <"Аккордеон" меню>
+for(i=0; i<accordion.length; i++) {
+    let list=accordion[i].nextElementSibling;
+
+    accordion[i].addEventListener('click', function() {
+        this.classList.toggle('active');
+        list.classList.toggle('open');
+    });
+}
+// </"Аккордеон" меню>
 
 // =================================================================================
 
