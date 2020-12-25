83f9bde1ce07b89cff4d4f67c15d2030a3a29dd3
lkwazar <75472596+lkwazar@users.noreply.github.com>
2020-12-25 11:33:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,9 +7,19 @@ let isMobile = {
     any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.IOS() || isMobile.Opera() || isMobile.Windows() ); }
 };
 
+const body = document.querySelector('body');
+
+if(isMobile.any()) {
+    body.classList.add('touch');
+}else{
+    body.classList.add('mouse');
+}
+
+// =================================================================================
+
+// <Меню по клику>
 const btnBurger = document.querySelector('.main-menu__burger');
 const menu =  document.querySelector('.main-menu');
-const body = document.querySelector('body');
 const toggleMenu = function() {
     menu.classList.toggle('open');
 }
@@ -17,40 +27,42 @@ const toggleBurger = function() {
     btnBurger.classList.toggle('active');
 }
 
-if(isMobile.any()) {
-    body.classList.add('touch');
+function touchArrow () {
     let arrow=document.querySelectorAll('.arrow_touch');
 
-    btnBurger.addEventListener('click', function(e) {
-        e.stopPropagation();
-        toggleBurger();
-        toggleMenu();
-        body.classList.toggle('lock');
-        
-    });
+    if(isMobile.any()) {
+        btnBurger.addEventListener('click', function(e) {
+            e.stopPropagation();
+            toggleBurger();
+            toggleMenu();
+            body.classList.toggle('lock');
+            
+        });
+    }
 
     for(i=0; i<arrow.length; i++) {
         let thisLink=arrow[i].previousElementSibling;
         let subMenu=arrow[i].nextElementSibling;
         let thisArrow=arrow[i];
 
-        thisLink.classList.add('parent');
+        if(isMobile.any()) {
+            thisLink.classList.add('parent');
+        }
         
         arrow[i].addEventListener('click', function() {
             subMenu.classList.toggle('open');
             thisArrow.classList.toggle('active');
         });
     }
-
-}else{
-    body.classList.add('mouse');
 }
+touchArrow();
+// </Меню по клику>
 
 // =================================================================================
 
 // <Картинка (img) в фон (background-image)>
 function ibg() {
-    var ibg = document.querySelectorAll('.ibg');
+    let ibg = document.querySelectorAll('.ibg');
 
     for (i=0; i<ibg.length; i++) {
         if(ibg[i].querySelector('img')) {
@@ -58,29 +70,7 @@ function ibg() {
         }          
     }
 }
-
 ibg();
 // </Картинка (img) в фон (background-image)>
 
 // =================================================================================
-
-// <События при изменении окна браузера>
-// window.onresize = adaptiveFunction;
-
-// function adaptiveFunction() {
-//     let search = document.querySelector('.search-form');
-//     let switchPanel = document.querySelector('.main-menu__list');
-//     let headerBottom = document.querySelector('.header-bottom__row');
-
-//     if(window.innerWidth < 620) {
-//         // Не работает в IE - switchPanel.before(search);
-//         search.classList.add('done')
-//         switchPanel.insertAdjacentElement('beforeBegin', search);
-//     }else if(window.innerWidth > 620) {
-//         // Не работает в IE - headerBottom.append(search);
-//         headerBottom.insertAdjacentElement('beforeEnd', search);
-//     }
-// }
-
-// adaptiveFunction();
-// </События при изменении окна браузера>
